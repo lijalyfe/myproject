b403568fa7abb06c4ee62f57a7e6bae18c218ef6
lija_lyfe <lija6556@list.ru>
2023-08-26 22:15:42 +0500

    creating a new controller

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 def home(request):
     return render(request, 'home.html')
 
 def contact(request):
     return render(request, 'contact.html')
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, 'product_detail.html', {'product': product})
