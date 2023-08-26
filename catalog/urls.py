7785a588948e7aed2478d9c244c6912532cf569a
lija_lyfe <lija6556@list.ru>
2023-08-26 22:20:29 +0500

    add a route 'product_detail'

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import product_detail
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
+    path('product/<int:pk>/', product_detail, name='product_detail'),
 ]
