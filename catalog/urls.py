a9983f398af514e1c72ef6a89435798aec6d7c55
lija_lyfe <lija6556@list.ru>
2023-08-26 23:26:00 +0500

    change urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 from . import views
 from .views import product_detail
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('product/<int:pk>/', product_detail, name='product_detail'),
+    path('', BlogListView.as_view(), name='blog_list'),
+    path('create/', BlogCreateView.as_view(), name='blog_create'),
+    path('<slug>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('<slug>/update/', BlogUpdateView.as_view(), name='blog_update'),
+    path('<slug>/delete/', BlogDeleteView.as_view(), name='blog_delete'),
 ]
