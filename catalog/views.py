c2d683a385d189df1fb7c54d6b95aee05f4038f2
lija_lyfe <lija6556@list.ru>
2023-08-26 23:24:32 +0500

    change views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.urls import reverse_lazy
+
+from .models import Blog
+from .forms import BlogForm
 
 def home(request):
     return render(request, 'home.html')
@@ -10,3 +21,36 @@ def contact(request):
 def product_detail(request, pk):
     product = Product.objects.all()
     return render(request, 'home.html', {'product': product})
+
+class BlogListView(ListView):
+    model = Blog
+    context_object_name = 'blogs'
+    template_name = 'blog/blog_list.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Blog.objects.filter(published=True).order_by('-created_at')
+
+class BlogDetailView(DetailView):
+    model = Blog
+    context_object_name = 'blog'
+    template_name = 'blog/blog_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Blog
+    form_class = BlogForm
+    template_name = 'blog/blog_create.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    form_class = BlogForm
+    template_name = 'blog/blog_update.html'
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    template_name = 'blog/blog_delete.html'
+    success_url = reverse_lazy('blog_list')
