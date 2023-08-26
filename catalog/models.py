b3cf4c0fa7f2d2bf60645ee49f895bdb26ae172c
lija_lyfe <lija6556@list.ru>
2023-08-26 23:17:34 +0500

    add class Blog

--- catalog/models.py
+++ catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from PIL import Image
 from django.apps import apps
 from datetime import datetime
+from django.utils.text import slugify
 
 class Category(models.Model):
     # наименование категории
@@ -38,3 +39,21 @@ class Product(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     # дата последнего изменения записи о товаре
     updated = models.DateTimeField(auto_now=True)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    content = models.TextField()
+    preview = models.ImageField(upload_to='blog_previews/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    published = models.BooleanField(default=False)
+    views_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Blog, self).save(*args, **kwargs)
